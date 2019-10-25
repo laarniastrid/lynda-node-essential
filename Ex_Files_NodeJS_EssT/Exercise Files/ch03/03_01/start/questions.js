1e9cd1656aff22fbefaa0236f2b704f5fa892a81
Astrid Sutton <astrid.sutton@wgu.edu>
2019-10-25 15:16:18 -0600

    complete standard output,

---
+++ Ex_Files_NodeJS_EssT/Exercise Files/ch03/03_01/start/questions.js
@@ -0,0 +1,13 @@
+// process
+const questions = [
+  "what is your name?",
+  "what would you rather be doing?",
+  "what is your preferred programming language?"
+];
+
+const ask = (i = 0) => {
+  process.stdout.write(`\n\n\n ${questions[i]}`);
+  process.stdout.write(` > `);
+}
+
+ask();
