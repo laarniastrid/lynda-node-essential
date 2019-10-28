b8d77e368a6627be6373926b170349f89fd4703f
Astrid Sutton <astrid.sutton@wgu.edu>
2019-10-28 11:43:06 -0600

    completed standard input,

--- Ex_Files_NodeJS_EssT/Exercise Files/ch03/03_01/start/questions.js
+++ Ex_Files_NodeJS_EssT/Exercise Files/ch03/03_01/start/questions.js
@@ -11,3 +11,25 @@ const ask = (i = 0) => {
 }
 
 ask();
+
+const answers = [];
+process.stdin.on('data', data => {
+  answers.push(data.toString().trim());
+
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+process.on('exit', () => {
+  const [name, activity, lang] = answers;
+  console.log(`
+
+    thank you for your answers.
+
+    go ${activity} ${name} you can write ${lang} code later
+    
+  `)
+})
