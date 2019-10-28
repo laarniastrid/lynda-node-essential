b61f0960b49c140d8da805ce6e630561f1bc1bae
Astrid Sutton <astrid.sutton@wgu.edu>
2019-10-28 12:09:51 -0600

    completed setinterval,

--- Ex_Files_NodeJS_EssT/Exercise Files/ch03/03_01/start/timers.js
+++ Ex_Files_NodeJS_EssT/Exercise Files/ch03/03_01/start/timers.js
@@ -1,6 +1,18 @@
 const waitTime = 3000; // 3 sec
+const waitInterval = 500; // 0.5 sec
+let currentTime = 0;
+
+const incTime = () => {
+  currentTime += waitInterval;
+  console.log(`waiting ${currentTime / 1000} seconds`);
+};
+
 console.log(`setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log('done');
+const timerFinished = () => {
+  clearInterval(interval);
+  console.log('done')
+};
 
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
