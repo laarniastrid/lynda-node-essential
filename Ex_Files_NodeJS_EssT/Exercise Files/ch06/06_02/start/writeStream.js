a069173bd3506fdc27a36eccdf6b56c09897099b
Astrid Sutton <astrid.sutton@wgu.edu>
2019-11-04 11:14:30 -0700

    complete: writable file streams

---
+++ Ex_Files_NodeJS_EssT/Exercise Files/ch06/06_02/start/writeStream.js
@@ -0,0 +1,20 @@
+const fs = require ('fs');
+
+const writeStream = fs.createWriteStream('./assets/myFile.txt', 'UTF-8');
+const readStream = fs.createReadStream('./assets/lorum-ipsum.md', 'UTF-8');
+
+// process.stdout.write('hello');
+// process.stdout.write(' world\n');
+
+// writeStream.write('hello');
+// writeStream.write(' world\n');
+
+// process.stdin.on('data', data => {
+//   writeStream.write(data);
+// })
+
+readStream.on('data', data => {
+  writeStream.write(data);
+})
+
+// readStream.pipe(writeStream);
